9b79c9a0f7486ca0bff207b55b3c3c5a7d9157df
shanjian <shanjianli@gmail.com>
2026-08-10 21:14:44 +0000

    Add Users::ShareableLinkDialogComponent
    A flash-modal dialog for showing a one-time link (invitation or
    password-reset) with a one-click copy button, so an admin can share
    it via chat instead of relying on email. Used by two admin actions
    added in later commits.

---
+++ app/components/users/shareable_link_dialog_component.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module Users
+  # Renders a shareable link (invitation or password-reset) after an admin
+  # action, delivered via flash[:op_modal]. See OpModalFlashable.
+  class ShareableLinkDialogComponent < ApplicationComponent
+    include OpPrimer::ComponentHelpers
+
+    DIALOG_ID = "shareable-link-dialog"
+
+    def initialize(link:, title:, description:)
+      super()
+      @link = link
+      @title = title
+      @description = description
+    end
+  end
+end
---
+++ spec/components/users/shareable_link_dialog_component_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+require "spec_helper"
+
+RSpec.describe Users::ShareableLinkDialogComponent, type: :component do
+  let(:link) { "https://example.org/account/activate?token=abc123" }
+
+  before do
+    render_inline(described_class.new(link:, title: "Invitation link", description: "Share this with the user."))
+  end
+
+  it "renders as an auto-showing dialog" do
+    expect(page).to have_css("dialog##{described_class::DIALOG_ID}[data-controller='auto-show-dialog']", visible: :all)
+  end
+
+  it "shows the title" do
+    expect(page).to have_text("Invitation link")
+  end
+
+  it "shows the description" do
+    expect(page).to have_text("Share this with the user.")
+  end
+
+  it "renders the link in a clipboard-copy element" do
+    expect(page).to have_css("clipboard-copy[value='#{link}']", visible: :all)
+  end
+
+  it "renders a close button" do
+    expect(page).to have_button("Close")
+  end
+end
